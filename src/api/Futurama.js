9fb526a362535a70dde23cdddbcfa6178589284f
Abel Mettzner <mettznermello@gmail.com>
2026-06-08 21:49:03 -0300

    s

--- src/api/Futurama.js
+++ src/api/Futurama.js
@@ -1,7 +1,7 @@
 const BASE_URL = 'https://futuramaapi.com/api'
 
 export async function getCharacters() {
-  const response = await fetch(`${BASE_URL}/character`)
+  const response = await fetch(`${BASE_URL}/characters`)
 
   if (!response.ok) {
     throw new Error(`Erro ao buscar personagens: ${response.status}`)
